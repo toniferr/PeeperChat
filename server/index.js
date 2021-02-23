a88b74231b84b020b21d8432cee41bf8b55c191c
toni <ferreirocouto@gmail.com>
2021-02-23 20:21:31 +0100

    config socket.io

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ var app = express();
 var server = require('http').Server(app); //metodo http de node
 var io = require('socket.io')(server);
 
+app.use(express.static('client')); //todos los html de la carpeta client
+
 app.get('/chat', function(req, res){
     res.status(200).send('start chat');
+});
+
+io.on('connection', function(socket){
+    console.log("El cliente con ip: "+socket.handshake.address+ " se ha conectado");
 });
 
 server.listen(6677, function(){
